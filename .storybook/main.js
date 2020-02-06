c95b5ddadfbbaef3b34453b1b0f0f56c210ab11a
Daan van der Zwaag <daan@devign.it>
2020-02-06 11:35:13 +0100

    chore: add plugin 'addon-knobs' to Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,9 @@
 module.exports = {
+  // Add below the path from packages where stories are created
   stories: [
     '../packages/netlify-cms-core/src/**/*.stories.js',
     '../packages/netlify-cms-ui-default/src/**/*.stories.js',
+    '../packages/netlify-cms-backend-test/src/**/*.stories.js',
   ],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs'],
 };

a186d763ba7a2b721cd13ed248068fb45f63692b
danoszz <daan@devign.it>
2020-02-10 11:51:39 +0100

    chore: only search for Stories in UI package

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,5 @@
 module.exports = {
   // Add below the path from packages where stories are created
-  stories: [
-    '../packages/netlify-cms-core/src/**/*.stories.js',
-    '../packages/netlify-cms-ui-default/src/**/*.stories.js',
-    '../packages/netlify-cms-backend-test/src/**/*.stories.js',
-  ],
+  stories: ['../packages/netlify-cms-ui-default/src/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs'],
 };
